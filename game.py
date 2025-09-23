4f82d4d2e9d3a49a917a655bdb431a996b119111
Etienne BLANCHET <etienne.blt8@gmail.com>
2025-09-23 18:35:33 +0200

    Ajout des déplacements et de l'affichage

--- game.py
+++ game.py
@@ -1,24 +1,48 @@
 import pygame
+from player import Player
 
 class Game:
     # Initialisation des composants
     def __init__(self, screen):
         self.screen = screen              
         self.running = True               
-        self.clock = pygame.time.Clock()  
+        self.clock = pygame.time.Clock()
+        self.player = Player(0, 0)
 
     # Gestion des évènements
     def handling_events(self):
         for event in pygame.event.get():  
             # Gestion évènement de fermeture
             if event.type == pygame.QUIT: 
-                self.running = False    
+                self.running = False  
+
+        # Gestion des touches directionnelles
+        keys = pygame.key.get_pressed()
+
+        # Déplacement horizontal
+        if keys[pygame.K_LEFT]:
+            self.player.velocity[0] = -1
+        elif keys[pygame.K_RIGHT]:
+            self.player.velocity[0] = 1
+        else:
+            self.player.velocity[0] = 0
+
+        # Déplacement vertical
+        if keys[pygame.K_UP]:
+            self.player.velocity[1] = -1
+        elif keys[pygame.K_DOWN]:
+            self.player.velocity[1] = 1
+        else:
+            self.player.velocity[1] = 0  
 
     def update(self):
-        pass  # Ici on mettra la logique du jeu (déplacements, collisions…)
+        self.player.move()
+        self.player.animate()
 
     def display(self):
-        pass  # Ici on dessine tout sur l'écran
+        self.screen.fill("black")
+        self.player.draw(self.screen)
+        pygame.display.flip()
 
     # Boucle principale
     def run(self):
