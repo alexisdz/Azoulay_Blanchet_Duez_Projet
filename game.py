f44179d452666ed58e922a62ea283153a47d199a
Etienne BLANCHET <etienne.blt8@gmail.com>
2025-10-08 19:00:36 +0200

    Création des blocs de murs à gauche, au centre et à droite

--- game.py
+++ game.py
@@ -12,8 +12,27 @@ class Game:
         
         # Création des murs
         self.walls = pygame.sprite.Group()
-        self.wall = Wall(100, 100)
-        self.walls.add(self.wall)
+
+        # Murs de gauche
+        self.walls.add(Wall(120, screen_height * 3/4))
+        self.walls.add(Wall(120, screen_height * 3/4 + 32))
+        self.walls.add(Wall(152, screen_height * 3/4))
+        self.walls.add(Wall(184, screen_height * 3/4))
+        self.walls.add(Wall(184, screen_height * 3/4 + 32))
+
+        # Murs du milieu
+        self.walls.add(Wall(screen_width/2 - 48, screen_height * 3/4))
+        self.walls.add(Wall(screen_width/2 - 48, screen_height * 3/4 + 32))
+        self.walls.add(Wall(screen_width/2 - 16, screen_height * 3/4))
+        self.walls.add(Wall(screen_width/2 + 16, screen_height * 3/4))
+        self.walls.add(Wall(screen_width/2 + 16, screen_height * 3/4 + 32))
+
+        # Murs de droite
+        self.walls.add(Wall(screen_width - 152, screen_height * 3/4))
+        self.walls.add(Wall(screen_width - 152, screen_height * 3/4 + 32))
+        self.walls.add(Wall(screen_width - 184, screen_height * 3/4))
+        self.walls.add(Wall(screen_width - 216, screen_height * 3/4))
+        self.walls.add(Wall(screen_width - 216, screen_height * 3/4 + 32))
 
     # Gestion des évènements
     def handling_events(self):
@@ -52,6 +71,7 @@ class Game:
             False                # ne supprime pas le mur automatiquement
         )
 
+        # Mise à jour des murs lors d'une collision
         for _, hit_walls in collisions.items():
             for wall in hit_walls:
                 wall.update()
